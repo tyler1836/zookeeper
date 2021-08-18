871591c421ab1f971878794caa021862ae67c206
unknown <tyler.r.roll@gmail.com>
2021-08-18 09:21:15 -0600

    post validate

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming string data or array data
+app.use(express.urlencoded({extended: true}))
+//parse incoming JSON data
+app.use(express.json())
 const { animals } = require('./data/animals.json')
 
 
@@ -45,6 +51,33 @@ function findById (id, animalsArray){
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
+function createNewAnimal(body, animalsArray){
+    console.log(body);
+    // main code here
+    const animal = body;
+    animalsArray.push(animal)
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    //return finised code
+    return animal;
+}
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -60,4 +93,17 @@ app.get('/api/animals/:id', (req, res) =>{
    else{
        res.send(404)
    }
+})
+app.post('/api/animals', (req, res) =>{
+    // set id to last on array
+    req.body.id = animals.length.toString();
+    //validate data post
+    if(!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.')
+    }
+    else{
+    // add animal to json file and animals array
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal)
+    }
 })
